29b55ef990dd69bc79a10985f256476009e40321
pooyadhgh <pooya.dhgh@gmail.com>
2021-09-15 16:51:55 +0430

    Initialize the server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res, next) => {
+  res.send('Hello World!');
+});
+
+app.listen(process.env.PORT || 8080);
